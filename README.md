dce0c4e5e3ad72bdc39e68eacf57c031cc7654ae
duck <me@duck.me.uk>
2018-04-03 00:59:33 +0100

    Update (and generally fix) script with full PEP compliance, make the Runner a class instance (ala TycoonBuild style) and add the required `logging.cfg` file required for libopenttd to work properly.

--- README.md
+++ README.md
@@ -4,7 +4,14 @@ A super simple script for updating the subreddit's sidebar with current server i
 ## Howto
 This is a Python 2.7 project. Make sure you're using Python 2 in your shell before proceeding!
 
-It's also horribly broken, mainly because the import of `libopenttd-python2` with a git link causes setuptools to catch fire (things like missing config files, such as `logging.cfg`). Please feel free to fix this and pull request in!
+To run the updater, set the following environment variables:
+
+* `OTTD_RUNNER_APPID`: The Application ID of the Application to run against.
+* `OTTD_RUNNER_SECRET`: The Secret of the above Applcation.
+* `OTTD_RUNNER_USER`: The username of the account which owns the OAuth Application provided above
+* `OTTD_RUNNER_PASSWORD`: The password of the above account (in plaintext, sorry!)
+
+**The application will throw an AssertionError if any of these environment variables is not set.**
 
 ## Licensing
-The original author of this work is unknown, but it is available here with no intention of causing that author distress or financial harm. If you are the original author, please contact us, and we'll be more than happy to add attribution / properly license / remove!
+Please see [LICENSE](LICENSE).The original author of this work is unknown, but it is available here with no intention of causing that author distress or financial harm. If you are the original author, please contact us, and we'll be more than happy to add attribution / properly license / remove!
--- update.py
+++ update.py
@@ -1,68 +1,122 @@
+import time
+import os
+
+import itertools
+import praw
+import re
+import socket
 from openttd.client import Client, M_UDP
 from openttd.date import OpenTTDDate
-import praw, re, socket, requests, requests.auth
-#import urllib3
-#urllib3.disable_warnings()
-
-def processOffline (desc):
-    expr = re.compile('\[.*:[0-9]* - .*\]\(#status.*\)');
-    return expr.sub(processServer, desc);
-
-def processServer(m):
-    print 'Processing: ' + m.group() + '\n'
-    ip = re.search('\[(.*):([0-9]*)', m.group());
-    if ip:
-        if checkStatus(ip.group(1), ip.group(2)):
-            port = int(ip.group(2))
-            client = Client(ip.group(1), port)
-            client.connect(M_UDP)
-            gameInfo = client.getGameInfo()
-            client.disconnect()
-            if gameInfo is None:
-                return '[' + ip.group(1) + ':' + ip.group(2) + ' - online](#status-online)'
-            currentDate = OpenTTDDate(gameInfo.game_date)
-
-            return '[' + ip.group(1) + ':' + ip.group(2) + ' - ' + unicode(gameInfo.clients_on) + '/' + unicode(gameInfo.clients_max) + ' (' + unicode(currentDate.toYMD()[0]) + ')](#status-online)'
-        return '[' + ip.group(1) + ':' + ip.group(2) + ' - offline](#status-offline)'
-    return m.group()
-
-def checkStatus (ip, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect((ip, int(port)))
-        s.shutdown(2)
-        return True
-    except:
-        return False
 
 
+class Runner:
+    def processoffline(self, desc):
+        """
+        Parse the given text field for servers.
+        :param desc: Subreddit description
+        :return: string: New subreddit description
+        """
+        expr = re.compile('\[.*:[0-9]* - .*\]\(#status.*\)')
+        return expr.sub(self.processserver, desc)
+
+    def processserver(self, m):
+        """
+        Process the given server string.
+        :param m: Server string
+        :return: string: New server string
+        """
+        print('Processing: ' + m.group())
+        ip = re.search('\[(.*):([0-9]*)', m.group())
+        if ip:
+            if not self.checkstatus(ip.group(1), ip.group(2)):
+                # we're not up
+                print('{host}:{port} is offline'.format(host=ip.group(1), port=ip.group(2)))
+                return '[' + ip.group(1) + ':' + ip.group(2) + ' - offline](#status-offline)'
+            else:
+                # we are up, let's try and pull some data
+                port = int(ip.group(2))
+                client = Client(ip.group(1), port)
+                client.connect(M_UDP)
+                gameinfo = client.getGameInfo()
+                client.disconnect()
+                if gameinfo is None:
+                    # game info isn't available for some reason, but we are up
+                    print('{host}:{port} is online, but unable to get gameinfo'.format(
+                        host=ip.group(1),
+                        port=ip.group(2)
+                        )
+                    )
+                    return '[' + ip.group(1) + ':' + ip.group(2) + ' - online](#status-online)'
+                currentdate = OpenTTDDate(gameinfo.game_date)
+                # revision = gameInfo.server_revision
+                # we're up and have data, let's return a formatted string
+                print('{host}:{port} is online'.format(host=ip.group(1), port=ip.group(2)))
+                return '[{host}:{port} - {clients_on}/{clients_max} ({current_year})](#status-online)'.format(
+                    host=ip.group(1),
+                    port=ip.group(2),
+                    clients_on=gameinfo.clients_on,
+                    clients_max=gameinfo.clients_max,
+                    current_year=currentdate.toYMD()[0]
+                )
+
+        # this doesn't appear to be a properly formatted address, return what we got
+        return m.group()
+
+    @staticmethod
+    def checkstatus(ip, port):
+        """
+        Check whether the given ip and port are connectable.
+        Note: This is a socket check only, it doesn't check whether OpenTTD is running at the given address!
+        :param ip: IP or hostname
+        :param port: Port number
+        :return: boolean: is the server up?
+        """
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5)
+        try:
+            s.connect((ip, int(port)))
+            s.shutdown(2)
+            return True
+        except (socket.error, socket.herror, socket.gaierror):
+            # timeouts, other errors, that kind of thing
+            return False
+
+    @classmethod
+    def run(cls, runner_instance):
+        """
+        Execute the runner routine against the given runner instance.
+        :param runner_instance: An instance of Runner
+        :return: none
+        """
+        r = praw.Reddit(user_agent='linux:r_openttd_status_script:3.0 (/u/efess, /u/luaduck)',
+                        client_id=runner_instance.redditapp['app'],
+                        client_secret=runner_instance.redditapp['secret'],
+                        username=runner_instance.reddituser['username'],
+                        password=runner_instance.reddituser['password'])
+        subreddit = r.subreddits.search_by_name('openttd')[0]
+        settings = subreddit.mod.settings()
+
+        subreddit.mod.update(description=runner_instance.processoffline(settings['description']))
+
+        print("Sidebar update successful")
+
+    def __init__(self):
+        self.reddituser = {
+            'username': os.environ.get('OTTD_RUNNER_USER', None),
+            'password': os.environ.get('OTTD_RUNNER_PASS', None)
+        }
+        self.redditapp = {
+            'app': os.environ.get('OTTD_RUNNER_APPID', None),
+            'secret': os.environ.get('OTTD_RUNNER_APPSECRET', None)
+        }
+
+        # validate that all entries have been given in the laziest way possible
+        for k, v in itertools.chain(self.reddituser.iteritems(), self.redditapp.iteritems()):
+            assert v is not None, "A required configuration value is missing. Please check your environment variables."
 
 
-def getAccessToken():
-    response = requests.post("https://www.reddit.com/api/v1/access_token",
-                             # client id and client secret are obtained via your reddit account
-                             auth = requests.auth.HTTPBasicAuth("username", "password"),
-                             # provide your reddit user id and password
-                             data = {"grant_type": "password", "username": "username", "password":"targetpassword"},
-
-                             # you MUST provide custom User-Agent header in the request to play nicely with Reddit API guidelines
-                             headers = {"User-Agent": "linux:r_openttd_status_script:2.0 (updated by /u/efess and /u/luaduck)",
-                                        "Content-Type": "application/x-www-form-urlencoded"})
-    response = dict(response.json())
-
-    return response["access_token"]
-# app key: OFWw3I3nCAXglg
-# secret: fG2OGdYjdygXqSxugHRiZ0uGWgU
-
-#<platform>:<app ID>:<version string> (by /u/<reddit username>)
-r = praw.Reddit(user_agent='linux:r_openttd_status_script:2.0 (updated by /u/efess)')
-#r = praw.Reddit(user_agent='r_openttd_status_bot')
-
-r.set_oauth_app_info("appname","secret", "http://www.google.com")
-r.set_access_credentials(set(["modconfig","identity"]), getAccessToken())
-#print r.get_me()
-subreddit = r.get_subreddit('openttd')
-settings = subreddit.get_settings()
-kwargs = { 'description': processOffline(settings['description']) }
-
-subreddit.update_settings(**kwargs);
+if __name__ == '__main__':
+    runner = Runner()
+    while True:
+        Runner.run(runner)
+        time.sleep(60)
